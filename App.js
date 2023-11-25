eec648da2cf913b6c37e4a38abad6c98bc34f942
Ruben Tanahara <rmartinez@gdlconnect.com>
2023-11-25 11:45:48 -0600

    update App.js

--- App.js
+++ App.js
@@ -1,112 +1,112 @@
-import React, { useEffect, useState } from "react";
-import { ActivityIndicator, SafeAreaView, Text, View } from "react-native";
-import styles from "./App.styles";
-import OpenEndedQuestion from "./src/components/OpenEndedQuestion/OpenEndedQuestion";
-import ImageMultipleChoiceQuestion from "./src/components/ImageMultipleChoiceQuestion/ImageMultipleChoiceQuestion";
-import questions from "./assets/data/allQuestions";
-import Header from "./src/components/Header/Header";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import React, { useEffect, useState } from "react";
+import { ActivityIndicator, SafeAreaView, View } from "react-native";
+import styles from "./App.styles";
+import questions from "./assets/data/allQuestions";
 import FillTheBlank from "./src/components/FillTheBlank/FillTheBlank";
+import Header from "./src/components/Header/Header";
+import ImageMultipleChoiceQuestion from "./src/components/ImageMultipleChoiceQuestion/ImageMultipleChoiceQuestion";
+import OpenEndedQuestion from "./src/components/OpenEndedQuestion/OpenEndedQuestion";
 
 const App = () => {
-  const [currentQuestion, setCurrentQuestion] = useState(0);
-  const [selectedQuestion, setSelectedQuestion] = useState(
-    questions[currentQuestion],
-  );
-  const [lives, setLives] = useState(5);
-  const [hasLoaded, setHasLoaded] = useState(false);
-
-  useEffect(() => {
-    if (currentQuestion >= questions.length) {
-      alert("You have finished the quiz!");
-      setLives(5);
-      setCurrentQuestion(0);
-      return;
-    }
-    setSelectedQuestion(questions[currentQuestion]);
-  }, [currentQuestion]);
-
-  // load data from async storage
-  useEffect(() => {
-    LoadData();
-  }, []);
-  // save data to async storage
-  useEffect(() => {
-    if (!hasLoaded) {
-      return;
-    }
-    SaveData();
-  }, [lives, currentQuestion, hasLoaded]);
-
-  const onCorrectAnswer = () => {
-    setCurrentQuestion(currentQuestion + 1);
-  };
-
-  const restartQuiz = () => {
-    setCurrentQuestion(0);
-    setLives(5);
-  };
-
-  const onIncorrectAnswer = () => {
-    if (lives <= 1) {
-      alert("Game Over!", "Try again!");
-      restartQuiz();
-
-      return;
-    }
-    alert("Incorrect!");
-    setLives(lives - 1);
-  };
-
-  const SaveData = async () => {
-    await AsyncStorage.setItem("lives", lives.toString());
-    await AsyncStorage.setItem("currentQuestion", currentQuestion.toString());
-  };
-
-  const LoadData = async () => {
-    const lives = await AsyncStorage.getItem("lives");
-    const currentQuestion = await AsyncStorage.getItem("currentQuestion");
-    if (lives === null || currentQuestion === null) {
-      return;
-    }
-    setLives(parseInt(lives));
-    setCurrentQuestion(parseInt(currentQuestion));
-    setHasLoaded(true);
-  };
-  if (!hasLoaded) {
-    return (
-      <ActivityIndicator size="large" style={{ flex: 1 }} color="#FAD131" />
+    const [currentQuestion, setCurrentQuestion] = useState(0);
+    const [selectedQuestion, setSelectedQuestion] = useState(
+        questions[currentQuestion],
     );
-  }
-  return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <View style={styles.container}>
-        <Header progress={currentQuestion / questions.length} lives={lives} />
-        {selectedQuestion.type === "FILL_THE_BLANK" && (
-          <FillTheBlank
-            question={selectedQuestion}
-            onCorrectAnswer={onCorrectAnswer}
-            onIncorrectAnswer={onIncorrectAnswer}
-          />
-        )}
-        {selectedQuestion.type === "IMAGE_MULTIPLE_CHOICE" && (
-          <ImageMultipleChoiceQuestion
-            question={selectedQuestion}
-            onCorrectAnswer={onCorrectAnswer}
-            onIncorrectAnswer={onIncorrectAnswer}
-          />
-        )}
+    const [lives, setLives] = useState(5);
+    const [hasLoaded, setHasLoaded] = useState(false);
 
-        {selectedQuestion.type === "OPEN_ENDED" && (
-          <OpenEndedQuestion
-            question={selectedQuestion}
-            onCorrectAnswer={onCorrectAnswer}
-            onIncorrectAnswer={onIncorrectAnswer}
-          />
-        )}
-      </View>
-    </SafeAreaView>
-  );
+    useEffect(() => {
+        if (currentQuestion >= questions.length) {
+            alert('Ymu have finished the quiz!')
+            setLives(5);
+            setCurrentQuestion(0);
+            return;
+        }
+        setSelectedQuestion(questions[currentQuestion]);
+    }, [currentQuestion]);
+
+    // load data from async storage
+    useEffect(() => {
+        LoadData();
+    }, []);
+    // save data to async storage
+    useEffect(() => {
+        if (!hasLoaded) {
+            return;
+        }
+        SaveData();
+    }, [lives, currentQuestion, hasLoaded]);
+
+    const onCorrectAnswer = () => {
+        setCurrentQuestion(currentQuestion + 1);
+    };
+
+    const restartQuiz = () => {
+        setCurrentQuestion(0);
+        setLives(5);
+    };
+
+    const onIncorrectAnswer = () => {
+        if (lives <= 1) {
+            alert("Game Over!", "Try again!");
+            restartQuiz();
+
+            return;
+        }
+        alert("Incorrect!");
+        setLives(lives - 1);
+    };
+
+    const SaveData = async () => {
+        await AsyncStorage.setItem("lives", lives.toString());
+        await AsyncStorage.setItem("currentQuestion", currentQuestion.toString());
+    };
+
+    const LoadData = async () => {
+        const lives = await AsyncStorage.getItem("lives");
+        const currentQuestion = await AsyncStorage.getItem("currentQuestion");
+        if (lives === null || currentQuestion === null) {
+            return;
+        }
+        setLives(parseInt(lives));
+        setCurrentQuestion(parseInt(currentQuestion));
+        setHasLoaded(true);
+    };
+    if (!hasLoaded) {
+        return (
+            <ActivityIndicator size="large" style={{ flex: 1 }} color="#FAD131" />
+        );
+    }
+    return (
+        <SafeAreaView style={{ flex: 1 }}>
+            <View style={styles.container}>
+                <Header progress={currentQuestion / questions.length} lives={lives} />
+                {selectedQuestion.type === "FILL_THE_BLANK" && (
+                    <FillTheBlank
+                        question={selectedQuestion}
+                        onCorrectAnswer={onCorrectAnswer}
+                        onIncorrectAnswer={onIncorrectAnswer}
+                    />
+                )}
+                {selectedQuestion.type === "IMAGE_MULTIPLE_CHOICE" && (
+                    <ImageMultipleChoiceQuestion
+                        question={selectedQuestion}
+                        onCorrectAnswer={onCorrectAnswer}
+                        onIncorrectAnswer={onIncorrectAnswer}
+                    />
+                )}
+
+                {selectedQuestion.type === "OPEN_ENDED" && (
+                    <OpenEndedQuestion
+                        question={selectedQuestion}
+                        onCorrectAnswer={onCorrectAnswer}
+                        onIncorrectAnswer={onIncorrectAnswer}
+                    />
+                )}
+            </View>
+        </SafeAreaView>
+    );
 };
 
 export default App;
--- src/components/FillTheBlank/FillTheBlank.js
+++ src/components/FillTheBlank/FillTheBlank.js
@@ -5,94 +5,94 @@ import WordOption from "../WordOption";
 import styles from "./styles";
 
 const FillTheBlank = ({ question, onCorrectAnswer, onIncorrectAnswer }) => {
-  const [parts, setParts] = useState(question.parts);
+    const [parts, setParts] = useState(question.parts);
 
-  const onButtonPress = () => {
-    if (checkIfCorrect()) {
-      onCorrectAnswer();
-    } else {
-      onIncorrectAnswer();
-    }
-  };
+    const onButtonPress = () => {
+        if (checkIfCorrect()) {
+            onCorrectAnswer();
+        } else {
+            onIncorrectAnswer();
+        }
+    };
 
-  const checkIfCorrect = () => {
+    const checkIfCorrect = () => {
+        return (
+            parts.filter((part) => part.isBlank && part.selected !== part.text)
+                .length === 0
+        );
+    };
+
+    const addOptionToSelected = (option) => {
+        if (isSelected(option)) {
+            return;
+        }
+
+        const newParts = [...parts];
+        for (let i = 0; i < newParts.length; i++) {
+            if (newParts[i].isBlank && !newParts[i].selected) {
+                newParts[i].selected = option;
+                break;
+            }
+        }
+        setParts(newParts);
+    };
+
+    const removeSelectedAt = (index) => {
+        const newParts = [...parts];
+        newParts[index].selected = null;
+        setParts(newParts);
+    };
+
+    const isSelected = (option) => {
+        return (
+            parts.filter((part) => part.isBlank && part.selected === option).length >
+            0
+        );
+    };
+
+    const isReadyToCheck = () => {
+        return parts.filter((part) => part.isBlank && !part.selected).length > 0;
+    };
+
     return (
-      parts.filter((part) => part.isBlank && part.selected !== part.text)
-        .length === 0
+        <>
+            <Text style={styles.title}>Complete the sentence</Text>
+            <View style={styles.row}>
+                {parts.map((part, index) => {
+                    if (part.isBlank) {
+                        return (
+                            <View style={styles.blank}>
+                                {part.selected && (
+                                    <WordOption
+                                        word={part.selected}
+                                        onPress={() => removeSelectedAt(index)}
+                                    />
+                                )}
+                            </View>
+                        );
+                    } else {
+                        return <Text style={styles.text}>{part.text}</Text>;
+                    }
+                })}
+            </View>
+
+            <View style={styles.optionsContainer}>
+                {question.options.map((option) => (
+                    <WordOption
+                        word={option}
+                        isSelected={isSelected(option)}
+                        onPress={() => addOptionToSelected(option)}
+                    />
+                ))}
+            </View>
+
+            <Button
+                text="Check"
+                onPress={onButtonPress}
+                disabled={isReadyToCheck()}
+            />
+        </>
     );
-  };
-
-  const addOptionToSelected = (option) => {
-    if (isSelected(option)) {
-      return;
-    }
-
-    const newParts = [...parts];
-    for (let i = 0; i < newParts.length; i++) {
-      if (newParts[i].isBlank && !newParts[i].selected) {
-        newParts[i].selected = option;
-        break;
-      }
-    }
-    setParts(newParts);
-  };
-
-  const removeSelectedAt = (index) => {
-    const newParts = [...parts];
-    newParts[index].selected = null;
-    setParts(newParts);
-  };
-
-  const isSelected = (option) => {
-    return (
-      parts.filter((part) => part.isBlank && part.selected === option).length >
-      0
-    );
-  };
-
-  const isReadyToCheck = () => {
-    return parts.filter((part) => part.isBlank && !part.selected).length > 0;
-  };
-  console.log(question.options);
-  return (
-    <>
-      <Text style={styles.title}>Complete the sentence</Text>
-      <View style={styles.row}>
-        {parts.map((part, index) => {
-          if (part.isBlank) {
-            return (
-              <View style={styles.blank}>
-                {part.selected && (
-                  <WordOption
-                    word={part.selected}
-                    onPress={() => removeSelectedAt(index)}
-                  />
-                )}
-              </View>
-            );
-          } else {
-            return <Text style={styles.text}>{part.text}</Text>;
-          }
-        })}
-      </View>
-
-      <View style={styles.optionsContainer}>
-        {question.options.map((option) => (
-          <WordOption
-            word={option}
-            isSelected={isSelected(option)}
-            onPress={() => addOptionToSelected(option)}
-          />
-        ))}
-      </View>
-
-      <Button
-        text="Check"
-        onPress={onButtonPress}
-        disabled={isReadyToCheck()}
-      />
-    </>
-  );
 };
 
 export default FillTheBlank;
